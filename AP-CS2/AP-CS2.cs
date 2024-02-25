a298b2d13983cc7285b99fcca6ca773b6c824eba
Marcel Greter <marcel.greter@ocbnet.ch>
2024-02-25 04:08:31 +0100

    Add AP-CS2 utility
    Useful for development in order to reference
    to patched version of the game.dll to be able
    to access previously private stuff directly.

---
+++ AP-CS2/AP-CS2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+using Mono.Cecil;
+
+namespace MODCS2
+{
+    class APCS2
+    {
+
+        private static string ManagedPath;
+        private static string BepInExPath;
+
+        /*
+        private static Assembly Resolver(object sender, ResolveEventArgs args)
+        {
+            Assembly assemby = null;
+            var parts = args.Name.Split(",");
+            Console.WriteLine("  resolving: " + parts[0] + ".dll");
+            var fqdn = ManagedPath + "\\" + parts[0] + ".dll";
+            if (File.Exists(fqdn))
+            {
+                assemby = Assembly.LoadFile(fqdn);
+                Console.WriteLine("  resolved: " + assemby);
+            }
+            fqdn = BepInExPath + "\\" + parts[0] + ".dll";
+            if (File.Exists(fqdn))
+            {
+                assemby = Assembly.LoadFile(fqdn);
+                Console.WriteLine("  resolved: " + assemby);
+            }
+            return assemby;
+        }
+        */
+        
+        static int Main(string[] args)
+        {
+
+            // Seems not really required, as resolver works
+            // AppDomain domain = AppDomain.CurrentDomain;
+            // domain.AssemblyResolve += new ResolveEventHandler(Resolver);
+
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Mandatory managed game directory not given.");
+                return 2;
+            }
+
+            ManagedPath = Path.GetFullPath(args[0]);
+            BepInExPath = ManagedPath + "\\..\\..\\BepInEx\\core";
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Mandatory output assembly not given.");
+                return 2;
+            }
+
+            string outputPath = Path.GetFullPath(args[1]);
+
+            var resolver = new DefaultAssemblyResolver();
+            resolver.AddSearchDirectory(ManagedPath);
+            resolver.AddSearchDirectory(BepInExPath);
+            string assemblyPath = ManagedPath + @"\Game.dll";
+            Console.WriteLine("Reading " + assemblyPath);
+            AssemblyDefinition assembly = AssemblyDefinition.ReadAssembly(
+                assemblyPath, new ReaderParameters { AssemblyResolver = resolver });
+            Console.WriteLine("  read " + assembly);
+
+            for (int i = 2; i < args.Length; i++)
+            {
+                int lastBackslashPos = args[i].LastIndexOf('\\') + 1;
+                string path = args[i].Substring(0, lastBackslashPos);
+                string glob = args[i].Substring(lastBackslashPos,
+                                args[i].Length - lastBackslashPos);
+
+                foreach (var fname in Directory.GetFiles(path, glob))
+                {
+                    string fqpath = Path.GetFullPath(fname);
+                    Console.WriteLine(" loading " + fqpath);
+                    Assembly DLL = Assembly.LoadFile(fqpath);
+                    foreach (TypeInfo type in DLL.GetTypes())
+                    {
+                        Type classType = DLL.GetType(type.FullName, true);
+                        if (classType.GetMethod("Patch") is MethodInfo methodInfo)
+                        {
+                            object[] methodArguments = { assembly };
+                            Console.WriteLine("  executing " + type.FullName + " patch");
+                            methodInfo.Invoke(null, methodArguments);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Writing " + outputPath);
+
+            assembly.Write(outputPath);
+
+            Console.WriteLine("  written " + assembly);
+
+            return 0;
+        }
+    }
+}
